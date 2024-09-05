1b50b0536f91cc5c44249839d62d699b2d229e9f
Ann Kornilova <Kirbri@yandex.ru>
2024-09-05 15:16:38 +0300

    3. Каркас шахматной доски [#792 #136630] v3

--- chess/src/test/java/ru/job4j/chess/LogicTest.java
+++ chess/src/test/java/ru/job4j/chess/LogicTest.java
@@ -13,7 +13,9 @@ public class LogicTest {
     public void whenMoveThenFigureNotFoundException()
             throws FigureNotFoundException {
         Logic logic = new Logic();
-        FigureNotFoundException exception = assertThrows(FigureNotFoundException.class, () -> logic.move(Cell.C1, Cell.H6));
+        FigureNotFoundException exception = assertThrows(FigureNotFoundException.class, () -> {
+            logic.move(Cell.C1, Cell.H6);
+        });
         assertThat(exception.getMessage()).isEqualTo("Figure not found on the board.");
     }
 
@@ -23,7 +25,9 @@ public class LogicTest {
         Logic logic = new Logic();
         logic.add(new BishopBlack(Cell.A1));
         logic.add(new BishopBlack(Cell.B2));
-        OccupiedCellException exception = assertThrows(OccupiedCellException.class, () -> logic.move(Cell.A1, Cell.B2));
+        OccupiedCellException exception = assertThrows(OccupiedCellException.class, () -> {
+            logic.move(Cell.A1, Cell.B2);
+        });
         assertThat(exception.getMessage()).isEqualTo("Cell is occupied.");
     }
 
@@ -32,7 +36,9 @@ public class LogicTest {
             throws ImpossibleMoveException {
         Logic logic = new Logic();
         logic.add(new BishopBlack(Cell.C1));
-        ImpossibleMoveException exception = assertThrows(ImpossibleMoveException.class, () -> logic.move(Cell.C1, Cell.H1));
+        ImpossibleMoveException exception = assertThrows(ImpossibleMoveException.class, () -> {
+            logic.move(Cell.C1, Cell.H1);
+        });
         assertThat(exception.getMessage()).isEqualTo("Could not way by diagonal from C1 to H1");
     }
 }
